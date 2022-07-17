9002fe847eb06fe4902407943c623935fad109a9
M O N I S H <55673780+monishvm@users.noreply.github.com>
2022-07-17 21:50:28 +0530

    Changed Owner name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hosted by Monish :)
         </a>
       </header>
     </div>
