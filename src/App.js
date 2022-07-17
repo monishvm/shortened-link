50a57f09fa51869b55983c09d11a0b544ce302ad
M O N I S H <55673780+Monishvm@users.noreply.github.com>
2022-07-17 21:56:58 +0530

    Changed name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hosted by Monish :)
+          Hosted by Monish
         </a>
       </header>
     </div>
